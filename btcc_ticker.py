6020a157dd492059bbb1e5583b07e258615b0dec
Robin Guo <robin.guo.ch@gmail.com>
2016-11-29 17:35:05 +0800

    btcc ticker

---
+++ btcc_ticker.py
@@ -0,0 +1,6 @@
+import json
+import urllib2
+
+data = urllib2.urlopen('https://pro-data.btcc.com/data/pro/ticker').read()
+results = json.loads(data)
+urllib2.urlopen('https://maker.ifttt.com/trigger/btcc_ticker/with/key/dAQW2GcyRqhfLeXHFs1Tn5?value1=' + str(results['ticker']['Last']) + '&value2=' + str(results['ticker']['Volume']))
