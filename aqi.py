72b221534b3831ca35c651a95352d74b4f6e9c72
Robin Guo <robin.guo.ch@gmail.com>
2016-12-03 11:57:12 +0800

    set user-agent

--- aqi.py
+++ aqi.py
@@ -3,6 +3,11 @@
 import urllib2
 import json
 
-data = urllib2.urlopen('https://waqi.info/api/feed/@1451/obs.en.json').read()
+req = urllib2.Request('https://waqi.info/api/feed/@1451/now.json')
+req.add_header('user-agent', 'Mozilla/5.0')
+req.add_header('origin', 'http://aqicn.org')
+req.add_header('referer', 'http://aqicn.org/city/all/')
+data = urllib2.urlopen(req).read()
 result = json.loads(data)
+# print result
 urllib2.urlopen('https://maker.ifttt.com/trigger/aqi_beijing/with/key/dAQW2GcyRqhfLeXHFs1Tn5?value1=' + str(result['rxs']['obs'][0]['msg']['aqi']))
