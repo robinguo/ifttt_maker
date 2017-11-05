3e079e5daef7677bfcd91c2daa84dfe0a8373a98
Robin Guo <robin.guo.ch@gmail.com>
2017-11-05 17:58:40 +0800

    update btcc eth ticker request'

--- btcc_ticker.py
+++ btcc_ticker.py
@@ -1,6 +1,6 @@
 import json
 import urllib2
 
-data = urllib2.urlopen('https://data.btcchina.com/data/ticker?market=btccny').read()
+data = urllib2.urlopen('https://blockchain.info/ticker').read()
 results = json.loads(data)
-urllib2.urlopen('https://maker.ifttt.com/trigger/btcc_ticker/with/key/dAQW2GcyRqhfLeXHFs1Tn5?value1=' + str(results['ticker']['last']) + '&value2=' + str(results['ticker']['vol']))
+urllib2.urlopen('https://maker.ifttt.com/trigger/btcc_ticker/with/key/dAQW2GcyRqhfLeXHFs1Tn5?value1=' + str(results['USD']['last']) + '&value2=' + str(results['USD']['symbol']))
--- eth_ticker.py
+++ eth_ticker.py
@@ -1,6 +1,6 @@
 import json
 import urllib2
 
-data = urllib2.urlopen('https://yunbi.com/api/v2/tickers/ethcny.json').read()
+data = urllib2.urlopen('https://api.coinmarketcap.com/v1/ticker/ethereum/').read()
 results = json.loads(data)
-urllib2.urlopen('https://maker.ifttt.com/trigger/eth_ticker/with/key/dAQW2GcyRqhfLeXHFs1Tn5?value1=' + str(results['ticker']['last']) + '&value2=' + str(results['ticker']['vol']))
+urllib2.urlopen('https://maker.ifttt.com/trigger/eth_ticker/with/key/dAQW2GcyRqhfLeXHFs1Tn5?value1=' + str(results[0]['price_usd']) + '&value2=' + str(results[0]['price_usd']))
