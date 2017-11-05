356aabdfa5e0b4cdbf43ac01a024da0bf43c43ab
Robin Guo <robin.guo.ch@gmail.com>
2017-11-05 17:49:06 +0800

    update aqi request

--- aqi.py
+++ aqi.py
@@ -2,9 +2,10 @@
 
 import urllib2
 import json
+import time
 
-req = urllib2.Request('https://api.waqi.info/feed/beijing/?token=2c22270e805a1277556ece6875e209383ad86a75')
+req = urllib2.Request('http://feed.aqicn.org/xservices/refresh:1451?_' + str(time.time()))
 data = urllib2.urlopen(req).read()
 result = json.loads(data)
 # print result
-urllib2.urlopen('https://maker.ifttt.com/trigger/aqi_beijing/with/key/dAQW2GcyRqhfLeXHFs1Tn5?value1=' + str(result['data']['aqi']))
+urllib2.urlopen('https://maker.ifttt.com/trigger/aqi_beijing/with/key/dAQW2GcyRqhfLeXHFs1Tn5?value1=' + str(result['aqiv']))
